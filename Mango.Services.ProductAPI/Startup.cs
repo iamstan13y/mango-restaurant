d84c38c3b4808570199564ee370b656a42bda66a
iamstan13y <iamstan13y@gmail.com>
2021-07-28 12:17:05 +0200

    Configured DbContext for ProductAPI.

--- Mango.Services.ProductAPI/Startup.cs
+++ Mango.Services.ProductAPI/Startup.cs
@@ -1,7 +1,9 @@
+using Mango.Services.ProductAPI.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,8 @@ namespace Mango.Services.ProductAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
