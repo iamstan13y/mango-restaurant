5387456cdab4f1d43a7dbefaebed63bf7a57faed
iamstan13y <iamstan13y@gmail.com>
2021-07-29 20:18:13 +0200

    Seed Products in ProductAPI.

--- Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -15,5 +15,30 @@ namespace Mango.Services.ProductAPI.DbContexts
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Samosa",
+                Price = 15,
+                Description = "Shit",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
+            
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Cup Cake",
+                Price = 18,
+                Description = "Shit",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
+        }
     }
 }
