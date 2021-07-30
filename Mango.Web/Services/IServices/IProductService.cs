58b3760fe14a9f5dc840aeeb6160ddd687ba36fa
iamstan13y <iamstan13y@gmail.com>
2021-07-30 14:14:07 +0200

    Product Service in Mango Web Project.

--- Mango.Web/Services/IServices/IProductService.cs
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -9,7 +9,7 @@ namespace Mango.Web.Services.IServices
     public interface IProductService
     {
         Task<T> GetAllProductsAsync<T>();
-        Task<T> GetAllProductByIdAsync<T>(int id);
+        Task<T> GetProductByIdAsync<T>(int id);
         Task<T> CreateProductAsync<T>(ProductDto productDto);
         Task<T> UpdateProductAsync<T>(ProductDto productDto);
         Task<T> DeleteProductAsync<T>(int id);
