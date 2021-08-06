3a0a18eab638abcd434032699983a225243bacb0
iamstan13y <iamstan13y@gmail.com>
2021-08-06 11:53:13 +0200

    Added Application user model.

--- Mango.Services.Identity/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Mango.Services.Identity.DbContexts
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
