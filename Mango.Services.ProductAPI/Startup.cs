4cca8447f78bf40d894c124034901cac592f95a2
iamstan13y <iamstan13y@gmail.com>
2021-08-08 01:25:36 +0200

    Configured security in swagger gen.

--- Mango.Services.ProductAPI/Startup.cs
+++ Mango.Services.ProductAPI/Startup.cs
@@ -61,6 +61,32 @@ namespace Mango.Services.ProductAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mango.Services.ProductAPI", Version = "v1" });
+                c.EnableAnnotations();
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { 
+                    Description = @"Enter 'Bearer' [space] and your token",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header 
+                        },
+                        new List<string>()
+                    }
+                });
             });
         }
 
