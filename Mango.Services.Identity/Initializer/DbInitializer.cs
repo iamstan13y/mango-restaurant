43e0d065ba5d435b6dca1694a53cb7485bd0cb6d
iamstan13y <iamstan13y@gmail.com>
2021-08-07 23:52:31 +0200

    Updated DbInitializer.

---
+++ Mango.Services.Identity/Initializer/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.Identity.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void Initialize()
+        {
+            if (_roleManager.FindByNameAsync(SD.Admin).Result == null)
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Client)).GetAwaiter().GetResult();
+            }
+            else
+            {
+                return;
+            }
+
+            ApplicationUser adminUser = new ApplicationUser()
+            {
+                UserName = "iamstan13y@gmail.com",
+                Email = "iamstan13y@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "0771027274"
+            }
+        }
+    }
+}
