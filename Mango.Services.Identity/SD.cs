2a87cd632b2902255ee8106b8b9a2f3b42501c9f
iamstan13y <iamstan13y@gmail.com>
2021-08-07 22:05:11 +0200

    Configured clients in Identity Server.

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,7 @@ namespace Mango.Services.Identity
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope> { 
-                new ApiScope("Mango", "Mango Server"),
+                new ApiScope("mango", "Mango Server"),
                 new ApiScope(name: "read", displayName: "Read your data."),
                 new ApiScope(name: "write", displayName: "Write your data."),
                 new ApiScope(name: "delete", displayName: "Delete your data."),
@@ -38,6 +39,21 @@ namespace Mango.Services.Identity
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = {"read", "write", "profile"}
                 },
-            }
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = {new Secret("Secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:44379/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:44379/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "mango"
+                    }
+                },
+            };
     }
 }
