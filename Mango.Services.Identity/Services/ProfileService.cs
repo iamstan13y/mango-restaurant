cbef683d4095a85254eac83467367bbb87cc070e
iamstan13y <iamstan13y@gmail.com>
2021-08-16 12:32:05 +0200

    Profile service Dependency injection.

--- Mango.Services.Identity/Services/ProfileService.cs
+++ Mango.Services.Identity/Services/ProfileService.cs
@@ -1,5 +1,7 @@
 ﻿using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,20 @@ namespace Mango.Services.Identity.Services
 {
     public class ProfileService : IProfileService
     {
+        private readonly IUserClaimsPrincipalFactory<ApplicationUser> _userClaimsPrincipalFactory;
+        private readonly UserManager<ApplicationUser> _userMgr;
+        private readonly RoleManager<IdentityRole> _roleMgr;
+
+        public ProfileService(
+            UserManager<ApplicationUser> userMgr,
+            RoleManager<IdentityRole> roleMgr,
+            IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipalFactory)
+        {
+            _roleMgr = roleMgr;
+            _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
+            _userMgr = userMgr;
+        }
+
         public Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
             throw new NotImplementedException();
