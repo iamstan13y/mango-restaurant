f3dbb05c394557a36a07fef9f34e2af787e9b4f6
iamstan13y <iamstan13y@gmail.com>
2021-08-16 21:21:33 +0200

    Removed unnecessary stuff.

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -39,7 +39,6 @@ namespace Mango.Web.Controllers
         [Authorize]
         public async Task<IActionResult> Login()
         {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
             return RedirectToAction(nameof(Index));
         }
         public IActionResult Logout()
