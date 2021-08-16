200ccaaa4611bd723eb4233a6400d4e01525f9c0
iamstan13y <iamstan13y@gmail.com>
2021-08-16 14:48:47 +0200

    Profile Service - Get Profile Data.

--- Mango.Services.Identity/Services/ProfileService.cs
+++ Mango.Services.Identity/Services/ProfileService.cs
@@ -1,10 +1,13 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
+using IdentityModel;
 using Mango.Services.Identity.Models;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Mango.Services.Identity.Services
@@ -25,14 +28,37 @@ namespace Mango.Services.Identity.Services
             _userMgr = userMgr;
         }
 
-        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
-            throw new NotImplementedException();
+            string sub = context.Subject.GetSubjectId();
+            ApplicationUser user = await _userMgr.FindByIdAsync(sub);
+            ClaimsPrincipal userClaims = await _userClaimsPrincipalFactory.CreateAsync(user);
+
+            List<Claim> claims = userClaims.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+            claims.Add(new Claim(JwtClaimTypes.FamilyName, user.LastName));
+            claims.Add(new Claim(JwtClaimTypes.GivenName, user.FirstName));
+            if (_userMgr.SupportsUserRole)
+            {
+                IList<string> roles = await _userMgr.GetRolesAsync(user);
+                foreach(var roleName in roles)
+                {
+                    claims.Add(new Claim(JwtClaimTypes.Role, roleName));
+                    if(_roleMgr.SupportsRoleClaims)
+                    {
+                        IdentityRole role = await _roleMgr.FindByNameAsync(roleName); 
+                        if(role != null)
+                        {
+                            claims.AddRange(await _roleMgr.GetClaimsAsync(role));
+                        }
+                    }
+                }
+            }
         }
 
         public Task IsActiveAsync(IsActiveContext context)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
