4b9277558bd2c2514667cd33c90b23b708c32174
iamstan13y <iamstan13y@gmail.com>
2021-08-16 12:23:43 +0200

    Added Profile Service class.

---
+++ Mango.Services.Identity/Services/ProfileService.cs
@@ -0,0 +1,22 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.Identity.Services
+{
+    public class ProfileService : IProfileService
+    {
+        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
