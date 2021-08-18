8791b5d7d8df35c3aea8addc6d41475732b4968a
iamstan13y <iamstan13y@gmail.com>
2021-08-18 15:10:15 +0200

    Fixed misplaced files in Product API.

--- Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
+++ Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Mango.Services.ProductAPI.Models
+namespace Mango.Services.ProductAPI.Models.Dto
 {
-    public class Product
+    public class ProductDto
     {
-        [Key]
         public int ProductId { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Range(1, 100)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
