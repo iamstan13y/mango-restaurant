ac20a9f8b5e6322ecd4ea7ddc5268c910dbc202d
iamstan13y <iamstan13y@gmail.com>
2021-08-19 08:56:12 +0200

    Added Cart Header DTO.

---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
