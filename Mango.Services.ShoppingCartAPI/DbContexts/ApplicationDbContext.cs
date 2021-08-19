2cc62a84d5567f3c9b1a7abbd1f0337c8b894e87
iamstan13y <iamstan13y@gmail.com>
2021-08-19 09:24:39 +0200

    Shopping Cart API DB Contexts.

--- Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace Mango.Services.ShoppingCartAPI.DbContexts
 
         }
 
-        //public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
     }
 }
