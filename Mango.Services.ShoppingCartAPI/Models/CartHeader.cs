3cda7810a11f0905ecaa26f93004a834375d9d34
iamstan13y <iamstan13y@gmail.com>
2021-08-19 08:56:48 +0200

    Added Cart & Response DTO.

--- Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
+++ Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -8,7 +8,6 @@ namespace Mango.Services.ShoppingCartAPI.Models
 {
     public class CartHeader
     {
-        [Key]
         public int CartHeaderId { get; set; }
         public string UserId { get; set; }
         public string CouponCode { get; set; }
