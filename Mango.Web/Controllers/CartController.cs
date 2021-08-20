016c76e5d249c00e8eca283f3e90479a6f3552be
iamstan13y <iamstan13y@gmail.com>
2021-08-21 00:31:59 +0200

    Methods to load shopping cart.

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -1,5 +1,8 @@
-﻿using Mango.Web.Services.IServices;
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +21,31 @@ namespace Mango.Web.Controllers
             _cartService = cartService;
         }
 
-        public IActionResult CartIndex()
+        public async Task<IActionResult> CartIndex()
         {
-            return View();
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var res = await _cartService.GetCartByUserIdAsync<ResponseDto>(userId, accessToken);
+
+            CartDto cartDto = new();
+            if (res != null && res.IsSuccess)
+            {
+                cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(res.Result));
+            }
+
+            if (cartDto.CartHeader != null)
+            {
+                foreach (var detail in cartDto.CartDetails)
+                {
+                    cartDto.CartHeader.OrderTotal += (detail.Product.Price * detail.Count);
+                }
+            }
+            return cartDto;
         }
     }
 }
