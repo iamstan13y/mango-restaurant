04e0f2334258f89dd2e723a1a7803bb2bf739645
iamstan13y <iamstan13y@gmail.com>
2021-08-20 23:26:17 +0200

    FIxed Details Post bug!

--- Mango.Services.ShoppingCartAPI/Models/Product.cs
+++ Mango.Services.ShoppingCartAPI/Models/Product.cs
@@ -13,7 +13,7 @@ namespace Mango.Services.ShoppingCartAPI.Models
         public int ProductId { get; set; }
         [Required]
         public string Name { get; set; }
-        [Range(1, 100)]
+        [Range(1, 1000)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
