4720d7777cbf50d3e8f0ddbff1890371121dec4b
iamstan13y <iamstan13y@gmail.com>
2021-08-20 23:49:21 +0200

    Added Cart Controller.

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public IActionResult CartIndex()
+        {
+            return View();
+        }
+    }
+}
