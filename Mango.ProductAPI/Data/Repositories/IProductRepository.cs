0f8bd4663920403a32a0db13c985171c8649293d
iamstan13y <iamstan13y@gmail.com>
2022-01-04 09:23:57 +0200

    Product Repository Interface.

--- Mango.ProductAPI/Data/Repositories/IProductRepository.cs
+++ Mango.ProductAPI/Data/Repositories/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mango.ProductAPI.Data.Models.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ namespace Mango.ProductAPI.Data.Repositories
 {
     public interface IProductRepository
     {
-        
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
