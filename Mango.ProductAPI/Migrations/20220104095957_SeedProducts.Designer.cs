facc7a506709a04cd3cb612cef28ab2b74dca3f9
iamstan13y <iamstan13y@gmail.com>
2022-01-04 12:04:15 +0200

    Seed Products.

--- Mango.ProductAPI/Migrations/20220104095957_SeedProducts.Designer.cs
+++ Mango.ProductAPI/Migrations/20220104095957_SeedProducts.Designer.cs
@@ -3,14 +3,16 @@ using Mango.ProductAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Mango.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220104095957_SeedProducts")]
+    partial class SeedProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
