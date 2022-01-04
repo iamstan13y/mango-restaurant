0facf358a7f4eb566dbd3587e475ac974769ccde
iamstan13y <iamstan13y@gmail.com>
2022-01-04 14:34:10 +0200

    Product Repository Interface & DTOs.

---
+++ Mango.Web/Enums/ApiType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Enums
+{
+    public enum ApiType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+}
---
+++ Mango.Web/SD.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web
+{
+    public static class SD
+    {
+        public static string ProductAPIBase { get; set; }
+        public static ApiType ApiType { get; set; }
+    }
+}
