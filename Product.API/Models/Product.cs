51d9c269d45a30b3b6eb32085e857d14b4a133c7
iamstan13y <iamstan13y@gmail.com>
2022-01-04 08:15:46 +0200

    Product model.

---
+++ Product.API/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.API.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
