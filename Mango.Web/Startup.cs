c2c4e5380d2058bc8712ee2adee41da8c4f8d35d
iamstan13y <iamstan13y@gmail.com>
2022-01-06 15:16:36 +0200

    Setup Main Project to use Identity Server.

--- Mango.Web/Startup.cs
+++ Mango.Web/Startup.cs
@@ -31,6 +31,26 @@ namespace Mango.Web
             services.AddScoped<IProductService, ProductService>();
 
             services.AddControllersWithViews();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+               .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+               .AddOpenIdConnect("oidc", options => {
+                   options.Authority = Configuration["ServiceUrls:IdentityServer"];
+                   options.GetClaimsFromUserInfoEndpoint = true;
+                   options.ClientId = "mango";
+                   options.ClientSecret = "secret";
+                   options.ResponseType = "code";
+
+                   options.TokenValidationParameters.NameClaimType = "name";
+                   options.TokenValidationParameters.RoleClaimType = "role";
+                   options.Scope.Add("mango");
+                   options.SaveTokens = true;
+               });
+
             services.AddRazorPages();
         }
 
@@ -52,6 +72,8 @@ namespace Mango.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
