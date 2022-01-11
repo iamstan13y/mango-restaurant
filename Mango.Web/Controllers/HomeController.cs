b8fce856c70cb22f30595392fd1ab503073a7d3d
iamstan13y <iamstan13y@gmail.com>
2022-01-11 10:46:23 +0200

    Created Index Action Method.

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,29 @@ namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
+
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
-            return View();
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public HomeController()
+        {
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto> products = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>("");
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(products);
         }
     }
 }
