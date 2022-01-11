d14f13b705df3d1ebf3a21ba0ec6f1d1bf3723d4
iamstan13y <iamstan13y@gmail.com>
2022-01-11 09:45:06 +0200

    Mango.Web - Clean-up in Startup & static data.

--- Mango.Web/Startup.cs
+++ Mango.Web/Startup.cs
@@ -2,14 +2,10 @@ using Mango.Web.Services;
 using Mango.Web.Services.IServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mango.Web
 {
