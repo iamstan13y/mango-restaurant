eaf764922dc93d692955ed6e8ce4e8a80f95c73d
iamstan13y <iamstan13y@gmail.com>
2022-01-11 09:42:19 +0200

    Mango.Web - Clean-up in Services.

--- Mango.Web/Services/IServices/IProductService.cs
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -1,7 +1,4 @@
 ﻿using Mango.Web.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mango.Web.Services.IServices
@@ -9,9 +6,13 @@ namespace Mango.Web.Services.IServices
     public interface IProductService : IBaseService
     {
         Task<T> GetAllProductsAsync<T>(string token);
+
         Task<T> GetProductByIdAsync<T>(int id, string token);
+
         Task<T> CreateProductAsync<T>(ProductDto productDto, string token);
+
         Task<T> UpdateProductAsync<T>(ProductDto productDto, string token);
+
         Task<T> DeleteProductAsync<T>(int id, string token);
     }
 }
