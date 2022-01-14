2fab224e5419f761054a993124a346f2690659c0
iamstan13y <iamstan13y@gmail.com>
2022-01-14 13:02:53 +0200

    Models for ShoppingCart.API

---
+++ ShoppingCart.API/Models/CartHeader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
