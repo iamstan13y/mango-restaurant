ed373de7145f1c32eba850656e797756bdebecb4
iamstan13y <iamstan13y@gmail.com>
2022-01-14 15:13:16 +0200

    Add to Shopping Cart Repository Method.

--- ShoppingCart.API/Models/Repository/CartRepository.cs
+++ ShoppingCart.API/Models/Repository/CartRepository.cs
@@ -1,4 +1,7 @@
-﻿using ShoppingCart.API.Models.Dto;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.API.Models.Data;
+using ShoppingCart.API.Models.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +11,45 @@ namespace ShoppingCart.API.Models.Repository
 {
     public class CartRepository : ICartRepository
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CartRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
         public Task<bool> ClearCart(string userId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
         {
-            throw new NotImplementedException();
+            Cart cart = _mapper.Map<Cart>(cartDto);
+
+            var product = _context.Products.FirstOrDefault(u => u.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
+            if (product == null)
+            {
+                _context.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+                await _context.SaveChangesAsync();
+            }
+
+            var cartHeader = _context.CartHeaders
+                .FirstOrDefaultAsync(u => u.UserId == cart.CartHeader.UserId);
+
+            if (cartHeader == null)
+            {
+                _context.CartHeaders.Add(cart.CartHeader);
+                await _context.SaveChangesAsync();
+
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+
+                cart.CartDetails.FirstOrDefault().Product = null;
+                _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                await _context.SaveChangesAsync();
+            }
         }
 
         public Task<CartDto> GetCartByUserId(string userId)
