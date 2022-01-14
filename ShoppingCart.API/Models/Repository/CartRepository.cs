cf824c13fc0ab425ba430e63738ee4a6a06d21ab
iamstan13y <iamstan13y@gmail.com>
2022-01-14 14:39:18 +0200

    Cart Repository Interface for ShoppingCart.API

---
+++ ShoppingCart.API/Models/Repository/CartRepository.cs
@@ -0,0 +1,31 @@
+﻿using ShoppingCart.API.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Models.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShoppingCart.API/Models/Repository/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingCart.API.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Models.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartDto> GetCartByUserId(string userId);
+        Task<CartDto> CreateUpdateCart(CartDto cartDto);
+        Task<bool> RemoveFromCart(int cartDetailsId);
+        Task<bool> ClearCart(string userId);
+    }
+}
