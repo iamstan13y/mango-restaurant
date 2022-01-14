ab6be6f18053a06e86969f58fb4df34f19569efa
iamstan13y <iamstan13y@gmail.com>
2022-01-14 16:03:37 +0200

    Update Shopping Cart Repository Method.

--- ShoppingCart.API/Models/Repository/CartRepository.cs
+++ ShoppingCart.API/Models/Repository/CartRepository.cs
@@ -36,7 +36,7 @@ namespace ShoppingCart.API.Models.Repository
                 await _context.SaveChangesAsync();
             }
 
-            var cartHeader = _context.CartHeaders
+            var cartHeader = await _context.CartHeaders.AsNoTracking()
                 .FirstOrDefaultAsync(u => u.UserId == cart.CartHeader.UserId);
 
             if (cartHeader == null)
@@ -50,6 +50,28 @@ namespace ShoppingCart.API.Models.Repository
                 _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
                 await _context.SaveChangesAsync();
             }
+            else
+            {
+                var cartDetails = await _context.CartDetails.AsNoTracking().FirstOrDefaultAsync(
+                    u => u.ProductId == cart.CartDetails.FirstOrDefault().ProductId &&
+                    u.CartHeaderId == cartHeader.CartHeaderId);
+
+                if (cartDetails == null)
+                {
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = cartHeader.CartHeaderId;
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    cart.CartDetails.FirstOrDefault().Count += cartDetails.Count;
+                    _context.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            return _mapper.Map<CartDto>(cart);
         }
 
         public Task<CartDto> GetCartByUserId(string userId)
