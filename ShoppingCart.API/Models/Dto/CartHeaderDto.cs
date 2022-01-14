3dfbce73851747bae6d2df073850b29c38b44022
iamstan13y <iamstan13y@gmail.com>
2022-01-14 14:25:25 +0200

    DTOs for ShoppingCart.API

---
+++ ShoppingCart.API/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
