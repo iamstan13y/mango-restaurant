497dd19d4657da0aeed6d46062a2c8fb32175584
iamstan13y <iamstan13y@gmail.com>
2022-01-17 10:12:17 +0200

    Models for Shopping Cart API.

--- Mango.Web/SD.cs
+++ Mango.Web/SD.cs
@@ -5,6 +5,7 @@ namespace Mango.Web
     public static class SD
     {
         public static string ProductAPIBase { get; set; }
+        public static string ShoppingCartAPIBase { get; set; }
         public static ApiType ApiType { get; set; }
     }
 }
