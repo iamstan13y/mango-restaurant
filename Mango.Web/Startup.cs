398a39e4d066b417902e5365979328096f5776a6
iamstan13y <iamstan13y@gmail.com>
2022-01-19 11:55:09 +0200

    Configure Cart Service - II.

--- Mango.Web/Startup.cs
+++ Mango.Web/Startup.cs
@@ -22,10 +22,12 @@ namespace Mango.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHttpClient<IProductService, ProductService>();
+            services.AddHttpClient<ICartService, CartService>();
             SD.ProductAPIBase = Configuration["ServiceUrls:ProductAPI"];
             SD.ShoppingCartAPIBase = Configuration["ServiceUrls:ShoppingCartAPI"];
             
             services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICartService, CartService>();
 
             services.AddControllersWithViews();
 
