2d37d8daf360de76ca3ac8ea786b4f0a85081659
iamstan13y <iamstan13y@gmail.com>
2022-01-26 17:32:03 +0200

    Configured Cart Service in Mango.Web

---
+++ Mango.Web/Services/IServices/ICouponService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface ICouponService
+    {
+        Task<T> GetCoupon<T>(string couponCode, string token = null);
+    }
+}
--- Mango.Web/Startup.cs
+++ Mango.Web/Startup.cs
@@ -24,11 +24,14 @@ namespace Mango.Web
         {
             services.AddHttpClient<IProductService, ProductService>();
             services.AddHttpClient<ICartService, CartService>();
+            services.AddHttpClient<ICouponService, CouponService>();
             SD.ProductAPIBase = Configuration["ServiceUrls:ProductAPI"];
             SD.ShoppingCartAPIBase = Configuration["ServiceUrls:ShoppingCartAPI"];
+            SD.CouponAPIBase = Configuration["ServiceUrls:CouponAPI"];
             
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICartService, CartService>();
+            services.AddScoped<ICouponService, CouponService>();
 
             services.AddControllersWithViews();
 
