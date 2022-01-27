24112e23e1327cbb3e8b553df18e790d4fda389d
iamstan13y <iamstan13y@gmail.com>
2022-01-27 16:00:46 +0200

    Order Repository Interface.

---
+++ Mango.OrderAPI/Models/Repository/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.OrderAPI.Models.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+
+        }
+
+        public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+
+        }
+    }
+}
