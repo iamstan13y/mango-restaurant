a6ac61ddf508abbff398bb187763761357a7832e
iamstan13y <iamstan13y@gmail.com>
2022-01-27 15:40:39 +0200

    Models for Order.API

---
+++ Mango.OrderAPI/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.OrderAPI.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
