180e31d4eac226e801a58cb13baf9f4870f185d4
iamstan13y <iamstan13y@gmail.com>
2022-01-28 08:19:41 +0200

    Models for Azure Messaging.

---
+++ Mango.OrderAPI/Messages/AzureServiceBusConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.OrderAPI.Messages
+{
+    public class AzureServiceBusConsumer
+    {
+        private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
+        {
+            var messsage = args.Message;
+            var body = Encoding.UTF8.GetString(messsage.Body);
+
+            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+        }
+    }
+}
---
+++ Mango.OrderAPI/Messages/CartDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.OrderAPI.Messages
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public virtual ProductDto Product { get; set; }
+        public int Count { get; set; }
+    }
+}
