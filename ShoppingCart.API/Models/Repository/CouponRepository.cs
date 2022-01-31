b8debf230c1b70eb9c5fd0d0ceee8f00eaecef93
iamstan13y <iamstan13y@gmail.com>
2022-01-31 08:25:13 +0200

    Coupon Repository Interface.

---
+++ ShoppingCart.API/Models/Repository/CouponRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingCart.API.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Models.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        public Task<CouponDto> GetCoupon(string couponName)
+        {
+
+        }
+    }
+}
---
+++ ShoppingCart.API/Models/Repository/ICouponRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShoppingCart.API.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Models.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCoupon(string couponName);
+    }
+}
