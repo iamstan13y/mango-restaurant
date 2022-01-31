b61359b862bcfe73481b577acd8f4d4b71be5388
iamstan13y <iamstan13y@gmail.com>
2022-01-31 10:51:15 +0200

    Code clean-up.

--- ShoppingCart.API/Models/Repository/CouponRepository.cs
+++ ShoppingCart.API/Models/Repository/CouponRepository.cs
@@ -1,8 +1,6 @@
 ﻿using Newtonsoft.Json;
 using ShoppingCart.API.Models.Dto;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
