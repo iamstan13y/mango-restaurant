8d3dc2bfd9cc05fe86442c5460b26e354a1dd8e3
iamstan13y <iamstan13y@gmail.com>
2022-02-09 14:40:27 +0200

    Clean-up in API Gateway.

--- Mango.Gateway/Startup.cs
+++ Mango.Gateway/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mango.Gateway
 {
