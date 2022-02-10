0566e43e448dd48653243a9d86c4c43c3e4300de
iamstan13y <iamstan13y@gmail.com>
2022-02-10 10:26:07 +0200

    RabbitMQ Consumer - II.

--- Mango.OrderAPI/Messages/RabbitMQConsumer.cs
+++ Mango.OrderAPI/Messages/RabbitMQConsumer.cs
@@ -1,9 +1,12 @@
 ﻿using Mango.OrderAPI.Models.Repository;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -32,7 +35,26 @@ namespace Mango.OrderAPI.Messages
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            stoppingToken.ThrowIfCancellationRequested();
 
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(content);
+                HandleMessage(checkoutHeaderDto);
+
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+            
+            _channel.BasicConsume("checkoutqueue", false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessage(CheckoutHeaderDto checkoutHeaderDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
