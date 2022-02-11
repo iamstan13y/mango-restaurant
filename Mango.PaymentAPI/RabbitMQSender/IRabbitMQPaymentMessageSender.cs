06e07b50952a2ef5a541f13dc1986508ccdfdd6d
iamstan13y <iamstan13y@gmail.com>
2022-02-11 09:34:11 +0200

    Consume message from OrderAPI.

---
+++ Mango.PaymentAPI/RabbitMQSender/IRabbitMQPaymentMessageSender.cs
@@ -0,0 +1,13 @@
+﻿using Mango.MessageBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.PaymentAPI.RabbitMQSender
+{
+    public interface IRabbitMQPaymentMessageSender
+    {
+        void SendMessage(BaseMessage baseMessage, string queueName);
+    }
+}
